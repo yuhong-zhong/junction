5107c6fd169ba03ee54034c39b7b5a0f9c341e82
Yuhong Zhong <yz@cs.columbia.edu>
2024-11-02 20:49:13 +0000

    tweak to work, switch to non-direct

--- scripts/install_caladan.sh
+++ scripts/install_caladan.sh
@@ -5,7 +5,7 @@ set -xe
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 ROOT_DIR=${SCRIPT_DIR}/../
 CALADAN_DIR=${ROOT_DIR}/lib/caladan
-CALADAN_PATCHES_DIR=${ROOT_DIR}/lib/patches/caladan
+# CALADAN_PATCHES_DIR=${ROOT_DIR}/lib/patches/caladan
 
 # Install Linux packages
 sudo -E apt install -y make cmake pkg-config libnl-3-dev libnl-route-3-dev libnuma-dev uuid-dev libssl-dev libaio-dev libcunit1-dev libclang-dev libncurses-dev meson python3-pyelftools
@@ -15,10 +15,10 @@ git submodule update --init --recursive -f caladan
 
 # Apply patches
 cd $CALADAN_DIR/
-git am $CALADAN_PATCHES_DIR/*
+# git am $CALADAN_PATCHES_DIR/*
 
 # Install Caladan
 make submodules
 (cd ksched && make -j `nproc`)
 
-cat $CALADAN_PATCHES_DIR/* | sha256sum >  $CALADAN_DIR/../.caladan_installed_ver
+# cat $CALADAN_PATCHES_DIR/* | sha256sum >  $CALADAN_DIR/../.caladan_installed_ver
--- scripts/submodule_check.sh
+++ scripts/submodule_check.sh
@@ -8,18 +8,18 @@ GLIBC_PATCHES_DIR=${ROOT_DIR}/lib/patches/glibc
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
-prev=$(cat "$ROOT_DIR/lib/.caladan_installed_ver" 2>&1 || true)
-cur=$(cat "$CALADAN_PATCHES_DIR"/* | sha256sum)
+# prev=$(cat "$ROOT_DIR/lib/.caladan_installed_ver" 2>&1 || true)
+# cur=$(cat "$CALADAN_PATCHES_DIR"/* | sha256sum)
 
 err=0
 
-if [ "$prev" != "$cur" ]; then
-	echo -n -e "$RED"
-	echo "Patches for Caladan have been updated since last install"
-	echo "Please run scripts/install_caladan.sh to update"
-	echo -e "$NC"
-	err=1
-fi
+# if [ "$prev" != "$cur" ]; then
+# 	echo -n -e "$RED"
+# 	echo "Patches for Caladan have been updated since last install"
+# 	echo "Please run scripts/install_caladan.sh to update"
+# 	echo -e "$NC"
+# 	err=1
+# fi
 
 prev=$(cat "$ROOT_DIR/lib/.glibc_installed_ver" 2>&1 || true)
 cur=$(cat "$GLIBC_PATCHES_DIR"/* | sha256sum)
